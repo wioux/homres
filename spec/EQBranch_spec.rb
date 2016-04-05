6f2fd62e5d1c088afd87ea471e0bbb7a1daa6f02
Peter Woo <petersnowdonwoo@gmail.com>
2016-04-05 14:48:32 -0700

    Add pending tests for EQBranch

---
+++ spec/EQBranch_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+require 'EQBranch'
+
+RSpec.describe EQBranch do
+  describe "#prop_time" do
+    pending "it should be roughly equal to ||u-v||"
+  end
+
+  describe "#fire" do
+    pending "it should add weight to connected branches"
+
+    pending "it should decrease the branch's own membrane potential"
+
+    pending "it should reset the threshold potential"
+  end
+
+  describe "#update(t)" do
+    pending "should set membrane/threshold potential to the decayed values at time t"
+  end
+
+  describe "#add(weight)" do
+    pending "it should increase membrane potential"
+
+    pending "it should #fire if membrane potential gets high enough"
+
+    pending "it should #fire if membrane potential gets low enough"
+  end
+end
