91a6988786c954a2c17edad6eafbf8e2cbe0042f
Peter Woo <petersnowdonwoo@gmail.com>
2016-04-05 14:07:53 -0700

    Change instance variables to attributes. Remove draw logic. Fix whitespace

--- src/EQBranch.rb
+++ src/EQBranch.rb
@@ -13,34 +13,31 @@ class EQBranch
 
   def self.setconstants
     #hopefully we do not have to solve this one level deeper with k and d
-    @@mpr=0 #unused for the moment
-    @@k=0.9
-    @@d=0.9
-    @@logd=Math.log(@@d)
-    @@i1=(@@k/(@@k+Math.log(@@d)))
-    @@i2=(@@logd+@@k)/@@logd
-    @@i3=@@logd-@@k
+    @@mpr = 0 #unused for the moment
+    @@k = 0.9
+    @@d = 0.9
+    @@logd = Math.log(@@d)
+    @@i1 = @@k/(@@k + Math.log(@@d))
+    @@i2 = (@@logd + @@k)/@@logd
+    @@i3 = @@logd - @@k
   end
 
-  attr_accessor :u, :v, :color #draw vals
-  def initialize u,v,color = Gosu::Color::WHITE
-    @u=u
-    @v=v
-    @color=color
-    #in case we want them to change, we store these as lambdas, I don't know how slow it is, but whatever.
-    @@drawables<<[->(){branch.u},->(){branch.v},->(){branch.color}]
+  attr_accessor :u, :v
+  attr_accessor :t0, :thresht, :mpt
+  def initialize(u, v)
+    @u, @v = u, v
 
     #async variables
-    @t = @@eventqueue.current#the time at which the last calculation was performed,
+    @t0 = @@eventqueue.current#the time at which the last calculation was performed,
     @mpt = 0 #membrane potential at time t
     @thresht = 1 #threshold at time t
 
-    @prop_time=(u-v).length
-    @prop_time*=1+(rand+rand+rand+rand)/2.0
+    @prop_time = (u - v).length
+    @prop_time *= 1 + (rand+rand+rand+rand)/2.0
     ##this is not even remotely accurate for average branch length with a path with a random walk derivative,
     ##the goal is simply to create some poisson-ish noise.
 
-    @outputs=[]
+    @outputs = []
   end
 
   def addoutput branch,weight
@@ -48,26 +45,26 @@ class EQBranch
   end
 
   def setproc #only construct this once. Why not?
-    @event=Proc.new{onfire}
+    @event = Proc.new{onfire}
   end
 
   ##TODO:naturalize reset values.
   def onfire
-    @thresht=1
-    @mpt=-0.1
+    self.thresht = 1
+    self.mpt = -0.1
     @outputs.each do |x|
-      branch, weight=x
+      branch, weight = x
       branch.addweight weight, @@eventqueue.current##solves mp(t) = T(t) and adds a new fire event at t, if it exists
     end
   end
 
   ##This function pushes the membrane state to time t, where presumably there will be a discontinuity
   def updatemembrane t
-    dt=t-@t
-    c1=@thresht-@@i1*@mpt
-    @thresht=c1*Math::E**(-@@k*dt)+@@i1*@mpt*@@d**dt
-    @mpt*=@@d**(dt)
-    @t=t
+    dt = t - t0
+    c1 = thresht - @@i1*mpt
+    self.thresht = c1*Math::E**(-@@k*dt) + @@i1*mpt*@@d**dt
+    self.mpt *= @@d**(dt)
+    self.t0  =  t
   end
 
   ##This function adds a weighted input to my branch at time t. there are three steps to this.
@@ -76,67 +73,26 @@ class EQBranch
   def addweight weight, t
     updatemembrane t
     @@eventqueue.cancel @event
-    @mpt+=weight
-    if @mpt>@thresht
-      fire @t
-    elsif @mpt<0 #the condition might be stricter than this
+    self.mpt += weight
+    if mpt > thresht
+      fire t0
+    elsif mpt < 0 #the condition might be stricter than this
       #solve for when mp and T are equal, fire then
-      fire @t+solvespike
+      fire t0 + solvespike
     end
   end
 
   def solvespike
-    c1=@thresht-@@i1*@mpt
-    Math.log((c1/@mpt)*@@i2)/@@i3
+    c1 = thresht - @@i1*mpt
+    Math.log((c1/mpt)*@@i2)/@@i3
   end
 
   def fire t
-    @@eventqueue.insert @event, t+@prop_time
-  end
-
-  #Debug
-  def settime x
-    @t=x
-  end
-
-  def setthresh x
-    @thresht=x
-  end
-
-  def setmp x
-    @mpt=x
+    @@eventqueue.insert @event, t + @prop_time
   end
 
   def incr t
-    updatemembrane @t+t
-  end
-
-  def thresh
-    @thresht
-  end
-
-  def mp
-    @mpt
-  end
-
-  ##fucking draw logic, pls ignore
-
-
-
-
-  @@drawables=[]
-  #not exactly sure when this gets called, presumably before the first time EQBranch is instantiated, possibly
-  #immediately upon loading the class, can always just do the more canonical thing and write a lazy accessor.
-
-  def self.draw_all
-    @@drawables.each do |x|
-      a,b,c=x.map(&:call)
-      @@window.draw_line(a.x,a.y,c,b.x,b.y,c)
-    end
-  end
-
-  def self.setwindow window
-    @@window=window
+    updatemembrane t0 + t
   end
 
   def self.seteventqueue eventqueue
